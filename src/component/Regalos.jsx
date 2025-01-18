66470c3a5babf394501faa9ec78f764e8b264eae
Maurikunin <mauri.kunin17@gmail.com>
2025-01-18 16:59:16 -0300

    gift

--- src/component/Regalos.jsx
+++ src/component/Regalos.jsx
@@ -113,7 +113,7 @@ const Regalos = ({ imgCbu }) => {
           }}
         >
          
-          <img src="https://res.cloudinary.com/dqqbiacuz/image/upload/v1737229066/Gift_uwq4vy.png" alt="CBU" style={{ width: "100%", maxWidth: "300px" }} />
+          <img src="https://res.cloudinary.com/dqqbiacuz/image/upload/v1737230331/gift_rb3wtg.png" alt="CBU" style={{ width: "100%", maxWidth: "300px" }} />
           <Button
             onClick={handleClose}
             sx={{
